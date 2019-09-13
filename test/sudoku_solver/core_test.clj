3fcb19f7dcdad7c5016210eeab66a14c87a6338a
ethan <ethan@debian>
2019-09-13 12:56:23 -0500

    fix artifacts from renaming project

--- test/sudoku_solver/core_test.clj
+++ test/sudoku_solver/core_test.clj
@@ -1,6 +1,6 @@
-(ns todo-list.core-test
+(ns sudoku-solver.core-test
   (:require [clojure.test :refer :all]
-            [todo-list.core :refer :all]))
+            [sudoku-solver.core :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I fail."
